6ed9c9a398e3abd3c00121f49d9d004530672bcc
martyTF <marty_TF@protonmail.com>
2023-05-10 08:47:45 +0200

    s

--- script.js
+++ script.js
@@ -1,40 +1,13 @@
 window.addEventListener('load', function () {
-    setTimeout(function () {
-        document.getElementById('searchbox').focus();
-    }, 1000);
+  setTimeout(function () {
+    document.getElementById('searchbox').focus();
+  }, 500);
 });
 
+
 window.addEventListener('load', function () {
-  const buttons = document.querySelectorAll("#linksrow a");
-  const boxes = document.querySelectorAll(".box-shower");
-  
-  // set initial active category
-  
-  // attach click listeners to navbar buttons
-  for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function() {
-      // remove active class from all categories
-      for (let j = 0; j < boxes.length; j++) {
-        boxes[j].classList.remove("active");
-      }
-      document.getElementById("search").classList.add("active");
-      // add active class to clicked category
-      const id = buttons[i].getAttribute("id").replace("button_", "");
-      document.getElementById(id).classList.add("active");
-      // add hidden class to all categories except active
-      for (let j = 0; j < boxes.length; j++) {
-        if (!boxes[j].classList.contains("active")) {
-          boxes[j].classList.add("hidden");
-        }
-      }
-      // remove hidden class from active category after slide-out animation
-      setTimeout(() => {
-        for (let j = 0; j < boxes.length; j++) {
-          if (boxes[j].classList.contains("active")) {
-            boxes[j].classList.remove("hidden");
-          }
-        }
-      }, 1100);
-    });
-  }
+  var boxShower = document.querySelectorAll('.box-shower');
+  document.getElementById("button_search").addEventListener("click", function () {
+    alert("hello")
   });
+});
