1d6cf89dad8094fdaa866cee08a15a80ef0635dd
martyTF <marty_TF@protonmail.com>
2023-06-10 16:50:06 +0200

    made stuff work better also fixed git shit

--- script.js
+++ script.js
@@ -1,156 +1,128 @@
 window.addEventListener('load', function () {
-  document.getElementById("search").style.display = "block";
-  document.getElementById("search").style.animation = "appearcontent 0.5s ease";
-  document.getElementById("services").style.display = "none";
-  document.getElementById("about").style.display = "none";
-  document.getElementById("dotfiles").style.display = "none";
-  document.getElementById("server").style.display = "none";
-  document.getElementById("projects").style.display = "none";
-  document.getElementById("contact").style.display = "none";
-  document.getElementById("mainmain").style.overflowX = "hidden";
+  var x = document.getElementsByClassName("box-shower");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+    document.getElementById("search").style.display = "block";
+    document.getElementById("search").style.animation = "appearcontent 0.5s ease";
+  };
   setTimeout(function () {
     document.getElementById('searchbox').focus();
-  }, 500);
+  }, 200);
 });
 
 
 function button_search() {
-  document.getElementById("search").style.display = "block";
-  document.getElementById("search").style.animation = "appearcontent 0.5s ease";
-  document.getElementById("services").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("about").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("dotfiles").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("server").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("projects").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("contact").style.animation = "disappearcontent 0.5s ease";
-  setTimeout(function () {
+  var x = document.getElementsByClassName("box-shower");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.animation = "disappearcontent 0.5s ease";
     document.getElementById("search").style.display = "block";
-    document.getElementById("services").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("dotfiles").style.display = "none";
-    document.getElementById("server").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+    document.getElementById("search").style.animation = "appearcontent 0.5s ease";
+  };
+  setTimeout(function () {
+    var x = document.getElementsByClassName("box-shower");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      document.getElementById("search").style.display = "block";
+    };
     document.getElementById('searchbox').focus();
   }, 500);
+  setTimeout(function () {
+    document.getElementById('searchbox').focus();
+  }, 200);
 };
 
 function button_services() {
-  document.getElementById("services").style.display = "block";
-  document.getElementById("services").style.animation = "appearcontent 0.5s ease";
-  document.getElementById("search").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("about").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("dotfiles").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("server").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("projects").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("contact").style.animation = "disappearcontent 0.5s ease";
-  setTimeout(function () {
+  var x = document.getElementsByClassName("box-shower");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.animation = "disappearcontent 0.5s ease";
     document.getElementById("services").style.display = "block";
-    document.getElementById("search").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("dotfiles").style.display = "none";
-    document.getElementById("server").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+    document.getElementById("services").style.animation = "appearcontent 0.5s ease";
+  };
+  setTimeout(function () {
+    var x = document.getElementsByClassName("box-shower");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      document.getElementById("services").style.display = "block";
+    };
   }, 500);
 };
 
 function button_about() {
-  document.getElementById("about").style.display = "block";
-  document.getElementById("about").style.animation = "appearcontent 0.5s ease";
-  document.getElementById("search").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("services").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("dotfiles").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("server").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("projects").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("contact").style.animation = "disappearcontent 0.5s ease";
-  setTimeout(function () {
+  var x = document.getElementsByClassName("box-shower");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.animation = "disappearcontent 0.5s ease";
     document.getElementById("about").style.display = "block";
-    document.getElementById("search").style.display = "none";
-    document.getElementById("services").style.display = "none";
-    document.getElementById("dotfiles").style.display = "none";
-    document.getElementById("server").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+    document.getElementById("about").style.animation = "appearcontent 0.5s ease";
+  };
+  setTimeout(function () {
+    var x = document.getElementsByClassName("box-shower");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      document.getElementById("about").style.display = "block";
+    };
   }, 500);
 };
 
 function button_dotfiles() {
-  document.getElementById("dotfiles").style.display = "block";
-  document.getElementById("dotfiles").style.animation = "appearcontent 0.5s ease";
-  document.getElementById("search").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("about").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("services").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("server").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("projects").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("contact").style.animation = "disappearcontent 0.5s ease";
+  var x = document.getElementsByClassName("box-shower");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.animation = "disappearcontent 0.5s ease";
+    document.getElementById("dotfiles").style.display = "block";
+    document.getElementById("dotfiles").style.animation = "appearcontent 0.5s ease";
+  };
   setTimeout(function () {
-    document.getElementById("about").style.display = "block";
-    document.getElementById("search").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("services").style.display = "none";
-    document.getElementById("server").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+    var x = document.getElementsByClassName("box-shower");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      document.getElementById("dotfiles").style.display = "block";
+    };
   }, 500);
 };
 
 function button_server() {
-  document.getElementById("server").style.display = "block";
-  document.getElementById("server").style.animation = "appearcontent 0.5s ease";
-  document.getElementById("search").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("about").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("dotfiles").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("services").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("projects").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("contact").style.animation = "disappearcontent 0.5s ease";
-  setTimeout(function () {
+  var x = document.getElementsByClassName("box-shower");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.animation = "disappearcontent 0.5s ease";
     document.getElementById("server").style.display = "block";
-    document.getElementById("search").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("dotfiles").style.display = "none";
-    document.getElementById("services").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+    document.getElementById("server").style.animation = "appearcontent 0.5s ease";
+  };
+  setTimeout(function () {
+    var x = document.getElementsByClassName("box-shower");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      document.getElementById("server").style.display = "block";
+    };
   }, 500);
 };
 
 function button_projects() {
-  document.getElementById("projects").style.display = "block";
-  document.getElementById("projects").style.animation = "appearcontent 0.5s ease";
-  document.getElementById("search").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("about").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("dotfiles").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("server").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("services").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("contact").style.animation = "disappearcontent 0.5s ease";
-  setTimeout(function () {
+  var x = document.getElementsByClassName("box-shower");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.animation = "disappearcontent 0.5s ease";
     document.getElementById("projects").style.display = "block";
-    document.getElementById("search").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("dotfiles").style.display = "none";
-    document.getElementById("server").style.display = "none";
-    document.getElementById("services").style.display = "none";
-    document.getElementById("contact").style.display = "none";
+    document.getElementById("projects").style.animation = "appearcontent 0.5s ease";
+  };
+  setTimeout(function () {
+    var x = document.getElementsByClassName("box-shower");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      document.getElementById("projects").style.display = "block";
+    };
   }, 500);
 };
 
 function button_contact() {
-  document.getElementById("contact").style.display = "block";
-  document.getElementById("contact").style.animation = "appearcontent 0.5s ease";
-  document.getElementById("search").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("about").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("dotfiles").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("server").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("projects").style.animation = "disappearcontent 0.5s ease";
-  document.getElementById("services").style.animation = "disappearcontent 0.5s ease";
-  setTimeout(function () {
+  var x = document.getElementsByClassName("box-shower");
+  for (var i = 0; i < x.length; i++) {
+    x[i].style.animation = "disappearcontent 0.5s ease";
     document.getElementById("contact").style.display = "block";
-    document.getElementById("search").style.display = "none";
-    document.getElementById("about").style.display = "none";
-    document.getElementById("dotfiles").style.display = "none";
-    document.getElementById("server").style.display = "none";
-    document.getElementById("projects").style.display = "none";
-    document.getElementById("services").style.display = "none";
+    document.getElementById("contact").style.animation = "appearcontent 0.5s ease";
+  };
+  setTimeout(function () {
+    var x = document.getElementsByClassName("box-shower");
+    for (var i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      document.getElementById("contact").style.display = "block";
+    };
   }, 500);
 };
