ba573fac22bcf1230b7d8196db0b826646f3a8a4
martyTF <marty_TF@protonmail.com>
2023-09-27 11:09:21 +0200

    swaggy anims lmaoooo

--- script.js
+++ script.js
@@ -23,9 +23,18 @@ function getCookie(cname) {
 };
 
 window.addEventListener('load', function () {
-  document.getElementById("search").style.display = "block";
-  document.getElementById("search").style.animation = "appearcontent 1s ease";
-  document.getElementById("searchfootnote").style.animation = "appearfooter 1s ease";
+  var x = document.getElementsByClassName("box-shower");
+  for (var i = 0; i < x.length; i++)
+    {
+      x[i].style.height = "0px";
+      x[i].style.overflow = "hidden";
+    };
+  var srch = document.getElementById("search");
+  srch.style.height = "auto";
+  srch.style.overflow = "auto";
+  var srchh = srch.offsetHeight;
+  var srchh = srchh + "px";
+  srch.style.height = srchh;
   setTimeout(function () {
     document.getElementById('searchbox').focus();
   }, 200);
@@ -45,29 +54,55 @@ window.addEventListener('load', function () {
 
 function shiftcontent(content) {
   var x = document.getElementsByClassName("box-shower");
-  for (var i = 0; i < x.length; i++) {
-    x[i].style.animation = "disappearcontent 1s ease";
+  var cont = document.getElementById(content);
+  cont.style.height = "auto";
+  var contheight = cont.offsetHeight;
+  cont.style.height = "0px";
+  for (var i = 0; i < x.length; i++)
+  {
+    x[i].style.height = "0px";
+    x[i].style.overflow = "hidden";
   };
-  document.getElementById(content).style.display = "block";
-  document.getElementById(content).style.animation = "appearcontent 1s ease";
-  setTimeout(function () {
-    var x = document.getElementsByClassName("box-shower");
-    for (var i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
-    };
-    document.getElementById(content).style.display = "block";
+  setTimeout(function() {
+    contheight = contheight + "px";
+    cont.style.height = contheight;
+    cont.overflow = "auto";
   }, 500);
   if (content == "search") {
-    setTimeout(function () {
+    setTimeout(function ()
+    {
       document.getElementById('searchbox').focus();
-    }, 200);
-    document.getElementById("searchfootnote").style.animation = "appearcontent 1s ease";
-  } 
+    }, 700);
+  }
   else { 
     return ""; 
   }
 };
 
+
+function toggleld(ld) {
+  let clrs = document.body.style;
+  if (ld == light) {
+    document.getElementById("darkmode").style.display = "inline";
+    document.getElementById("lightmode").style.display = "none";
+    clrs.setProperty('--mtf-background-color', "#eff1f5");
+    clrs.setProperty('--mtf-font-color', "#4c4f69");
+    clrs.setProperty('--mtf-background-2-color', "#e6e9efdf");
+    clrs.setProperty('--mtf-accent-color', "#dc8a78");
+    clrs.setProperty('--mtf-background-image', "url('src/Clearnight.jpg')");
+    setCookie("darklightc", 0, 30);
+  }
+  else {
+    document.getElementById("darkmode").style.display = "none";
+    document.getElementById("lightmode").style.display = "inline";
+    clrs.setProperty('--mtf-background-color', "#1e1e2e");
+    clrs.setProperty('--mtf-font-color', "#cdd6f4");
+    clrs.setProperty('--mtf-background-2-color', "#181825df");
+    clrs.setProperty('--mtf-accent-color', "#f5e0dc");
+    clrs.setProperty('--mtf-background-image', "url('src/Clearday.jpg')");
+    setCookie("darklightc", 1, 30);
+  }
+}
 
 function button_lightmode() {
   document.getElementById("darkmode").style.display = "inline";
@@ -90,4 +125,16 @@ function button_darkmode() {
   document.body.style.setProperty('--mtf-accent-color', "#f5e0dc");
   document.body.style.setProperty('--mtf-background-image', "url('src/Clearday.jpg')");
   setCookie("darklightc", 1, 30);
+}
+
+function bookmarks_open() {
+  let bmks = document.getElementById("bookmarks");
+  bmks.style.display = "block";
+  bmks.style.opacity = "1"
+}
+
+function bookmarks_close(){
+  let bmks = document.getElementById("bookmarks");
+  bmks.style.display = "none";
+  bmks.style.opacity = "0";
 }
